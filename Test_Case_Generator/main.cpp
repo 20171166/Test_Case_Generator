8caf73eff51deae98a2958c1b6b24fcbcdda78de
Seung won <laurent01@unist.ac.kr>
2020-07-21 17:28:43 +0900

    Initial Commit

---
+++ Test_Case_Generator/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test_Case_Generator
+//
+//  Created by Seung won Yoo on 2020/07/21.
+//  Copyright © 2020 Seung won Yoo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
