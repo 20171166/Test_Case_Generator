52f6148dcd09cbd0255f86e101bbb408b9eafe09
Seung won <laurent01@unist.ac.kr>
2020-07-21 17:55:06 +0900

    Add feature case

--- Test_Case_Generator/main.cpp
+++ Test_Case_Generator/main.cpp
@@ -5,22 +5,38 @@ using namespace std;
 
 int main() {
     
-    ofstream writeFILE;
-    writeFILE.open("/Users/laurent01/Desktop/kernel.txt",ios::in|ios::app);
-    if(writeFILE.is_open()){
+    ofstream writeF;
+    writeF.open("/Users/laurent01/Desktop/feature.txt",ios::in|ios::app);
+    if(writeF.is_open()){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 25; j++){
+                    stringstream stream;
+                    stream << dec << (i + 1) * (j + 1);
+                    string result(stream.str());
+                    writeF << result << "\n";
+            }
+        }
+    }
+    else cout << "ERROR : File is not open" << endl;
+    writeF.close();
+
+    
+    ofstream writeK;
+    writeK.open("/Users/laurent01/Desktop/kernel.txt",ios::in|ios::app);
+    if(writeK.is_open()){
         for(int i = 0; i < 3; i++){
             for(int j = 0; j < 9; j++){
                 for(int k = 0; k < 4; k++){
                     stringstream stream;
                     stream << dec << (k+1);
                     string result(stream.str());
-                    writeFILE << result << "\n";
+                    writeK << result << "\n";
                 }
             }
         }
     }
     else cout << "ERROR : File is not open" << endl;
-    writeFILE.close();
+    writeK.close();
 
     
 
