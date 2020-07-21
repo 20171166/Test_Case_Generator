6ead1fd2787cd2d82fec81418eed1248005cef7d
Seung won <laurent01@unist.ac.kr>
2020-07-21 17:41:15 +0900

    Test Case Generator Init

--- Test_Case_Generator/main.cpp
+++ Test_Case_Generator/main.cpp
@@ -1,15 +1,28 @@
-//
-//  main.cpp
-//  Test_Case_Generator
-//
-//  Created by Seung won Yoo on 2020/07/21.
-//  Copyright © 2020 Seung won Yoo. All rights reserved.
-//
-
 #include <iostream>
+#include <fstream>
+#include <sstream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int main() {
+    
+    ofstream writeFILE;
+    writeFILE.open("/Users/laurent01/Desktop/kernel.txt",ios::in|ios::app);
+    if(writeFILE.is_open()){
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 9; j++){
+                for(int k = 0; k < 4; k++){
+                    stringstream stream;
+                    stream << dec << (k+1);
+                    string result(stream.str());
+                    writeFILE << result << "\n";
+                }
+            }
+        }
+    }
+    else cout << "ERROR : File is not open" << endl;
+    writeFILE.close();
+
+    
+
     return 0;
 }
