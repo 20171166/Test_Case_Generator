1a2dd98ec5ee5f375f9b206a53b60dc2863b7b8d
Seung won <laurent01@unist.ac.kr>
2020-07-22 16:53:30 +0900

    Commit

--- Test_Case_Generator/main.cpp
+++ Test_Case_Generator/main.cpp
@@ -1,19 +1,22 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <cmath>
 using namespace std;
 
 int main() {
+    
+    unsigned int count = 0;
     
     ofstream writeF;
     writeF.open("/Users/laurent01/Desktop/feature.txt",ios::in|ios::app);
     if(writeF.is_open()){
         for(int i = 0; i < 25; i++){
-            for(int j = 0; j < 3; j++){
-                    stringstream stream;
-                    stream << dec << (i + 1) * (j + 1);
-                    string result(stream.str());
-                    writeF << result << "\n";
+            for(int j = 0; j < 128; j++){
+                stringstream stream;
+                stream << dec << (j + 1);
+                string result(stream.str());
+                writeF << result << "\n";
             }
         }
     }
@@ -24,11 +27,11 @@ int main() {
     ofstream writeK;
     writeK.open("/Users/laurent01/Desktop/kernel.txt",ios::in|ios::app);
     if(writeK.is_open()){
-        for(int i = 0; i < 3; i++){
+        for(int i = 0; i < 128; i++){
             for(int j = 0; j < 9; j++){
                 for(int k = 0; k < 4; k++){
                     stringstream stream;
-                    stream << dec << (1);
+                    stream << dec << (i + 1);
                     string result(stream.str());
                     writeK << result << "\n";
                 }
@@ -37,8 +40,6 @@ int main() {
     }
     else cout << "ERROR : File is not open" << endl;
     writeK.close();
-
-    
 
     return 0;
 }
